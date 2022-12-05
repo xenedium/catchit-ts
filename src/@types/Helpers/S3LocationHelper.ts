c0fe4c23368de668107fd7c94a37c86b31a8de4e
xenedium <ahmed@abderraziq.com>
2022-12-05 19:56:21 +0100

    Fix: image should be default url if not provided

--- src/@types/Helpers/S3LocationHelper.ts
+++ src/@types/Helpers/S3LocationHelper.ts
@@ -1,5 +1,5 @@
 export function S3LocationHelper (file?: Express.MulterS3.File): string | null {
-    if (!file) return null;
+    if (!file) return process.env.DEFAULT_USER_IMAGE;
     if (!process.env.S3_CDN_ENDPOINT) return file.location;
     return `${process.env.S3_CDN_ENDPOINT}/${file.key}`;
 }
