2a8279fb633f3cb6962cb9cec98167f446ce845f
xenedium <ahmed@abderraziq.com>
2022-12-18 18:11:32 +0100

    Disable eslint ignore

--- frontend/src/Hooks/useMyArticles.tsx
+++ frontend/src/Hooks/useMyArticles.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { useState, useEffect } from 'react';
 import { ArticleDto, HttpStatusCode, ServerJsonResponse } from '../@types';
 import { useSearchParams, useNavigate } from 'react-router-dom';
